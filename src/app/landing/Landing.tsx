e3d4f0d70115c21e90b80d285b81bedd1dfbb4da
Ethan Lee <ethan.lee@emory.edu>
2025-02-03 20:09:10 -0500

    Update landing page metadata for improved clarity and SEO optimization

--- src/app/landing/Landing.tsx
+++ src/app/landing/Landing.tsx
@@ -58,13 +58,12 @@ const Landing = () => {
 };
 
 const InvisibleHeader = () => {
-  const title = "Taehoon's Portfolio";
-  const description = `Hi there! I’m Taehoon Lee, a Computer Science student and aspiring
-        software developer who loves creating cool and impactful digital
-        experiences. From building AI-powered platforms to crafting interactive
-        web designs, I’m all about turning creative ideas into reality. Check
-        out my projects, explore my skills in Python, JavaScript, React, and
-        more, and let’s connect to build something amazing together!`;
+  const title = "Taehoon Lee - Creative Developer";
+  const description = `Computer Science and Psychology student at Emory University, 
+  passionate about building user-friendly, AI-powered, and innovative software. 
+  With experience at UrsaTech and eStreamly, I specialize in merging technology with 
+  human behavior to create intuitive, high-impact digital solutions. Explore my portfolio 
+  to see how I design cutting-edge software that enhances user experiences.`;
   const keywords = [
     "Creative Developer",
     "Portfolio",
@@ -74,23 +73,35 @@ const InvisibleHeader = () => {
     "Taehoon Lee",
     "Ethan",
     "Ethan Lee",
+    "Lee",
     "GitHub",
     "Software",
     "Developer",
     "Projects",
     "ethn1ee",
     "ethn1ee.github.io",
+    "Taehoon Portfolio",
+    "Taehoon Lee Portfolio",
+    "Ethan Portfolio",
+    "Ethan Lee Portfolio",
+    "Developer",
+    "Software",
+    "Computer Science",
+    "Psychology",
+    "Emory",
+    "이태훈",
+    "태훈",
   ];
 
   return (
     <div className="absolute top-0 left-0 hidden">
       <h1>{title}</h1>
       <h2>{description}</h2>
-      <h3>
+      <p>
         {keywords.map((keyword, index) => (
           <span key={index}>{keyword}, </span>
         ))}
-      </h3>
+      </p>
     </div>
   );
 };
