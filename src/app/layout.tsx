ac0beb31e0ce467a90469a9c03e657f296ecda1f
Ethan Lee <ethan.lee@emory.edu>
2025-02-03 20:17:30 -0500

    Refactor metadata handling by creating a separate Metadata component and updating InvisibleHeader to utilize it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,67 +1,13 @@
 import "./globals.css";
 
+import { metadataObject } from "@/components/Metadata";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Metadata } from "next";
 
 import { ABCFavorit } from "../components/Fonts";
 
-export const metadata: Metadata = {
-  title: "Taehoon Lee - Creative Developer",
-  description:
-    "Computer Science and Psychology student at Emory University, passionate about building user-friendly, AI-powered, and innovative software. With experience at UrsaTech and eStreamly, I specialize in merging technology with human behavior to create intuitive, high-impact digital solutions. Explore my portfolio to see how I design cutting-edge software that enhances user experiences.",
-  keywords: [
-    "Creative Developer",
-    "Portfolio",
-    "Web Development",
-    "UI/UX Design",
-    "Taehoon",
-    "Taehoon Lee",
-    "Ethan",
-    "Ethan Lee",
-    "Lee",
-    "GitHub",
-    "Software",
-    "Developer",
-    "Projects",
-    "ethn1ee",
-    "ethn1ee.github.io",
-    "Taehoon Portfolio",
-    "Taehoon Lee Portfolio",
-    "Ethan Portfolio",
-    "Ethan Lee Portfolio",
-    "Developer",
-    "Software",
-    "Computer Science",
-    "Psychology",
-    "Emory",
-    "이태훈",
-    "태훈",
-  ],
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: { index: true, follow: true },
-  },
-
-  openGraph: {
-    title: "Taehoon Lee - Creative Developer",
-    description:
-      "Computer Science and Psychology student at Emory University, passionate about building user-friendly, AI-powered, and innovative software. With experience at UrsaTech and eStreamly, I specialize in merging technology with human behavior to create intuitive, high-impact digital solutions. Explore my portfolio to see how I design cutting-edge software that enhances user experiences.",
-    url: "https://taehoonlee.dev/",
-    siteName: "Taehoon Lee - Creative Developer",
-    images: [
-      {
-        url: "https://taehoonlee.dev/taehoon.png",
-        width: 800,
-        height: 800,
-        alt: "Taehoon Lee",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-};
+export const metadata: Metadata = metadataObject;
 
 export default function RootLayout({
   children,
