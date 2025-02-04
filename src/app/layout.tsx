d656948a4050ea2a25e8eea2dde4d21b3d7c2ead
Ethan Lee <ethan.lee@emory.edu>
2025-02-03 19:52:01 -0500

    Update metadata and improve Open Graph settings in layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata: Metadata = {
     "Taehoon Lee",
     "Ethan",
     "Ethan Lee",
+    "Lee",
     "GitHub",
     "Software",
     "Developer",
@@ -37,15 +38,15 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Taehoon Lee - Creative Developer",
     description:
-      "I am a Computer Science and Psychology student at Emory University with a passion for creating user-friendly, innovative software. I love working at the crossroads of technology and human behavior, combining my technical skills with insights into how people think and interact. From building AI-powered platforms at UrsaTech to enhancing e-commerce experiences at eStreamly, I’ve developed a knack for creating solutions that are both impactful and intuitive.",
-    url: "https://www.taehoonlee.dev/",
+      "Computer Science and Psychology student at Emory University with a passion for creating user-friendly, innovative software. I love working at the crossroads of technology and human behavior, combining my technical skills with insights into how people think and interact. From building AI-powered platforms at UrsaTech to enhancing e-commerce experiences at eStreamly, I’ve developed a knack for creating solutions that are both impactful and intuitive.",
+    url: "https://taehoonlee.dev/",
     siteName: "Taehoon Lee - Creative Developer",
     images: [
       {
-        url: "https://www.taehoonlee.dev/",
+        url: "https://taehoonlee.dev/taehoon.png",
         width: 800,
         height: 800,
-        alt: "Taehoon Lee - Creative Developer",
+        alt: "Taehoon Lee",
       },
     ],
     locale: "en_US",
@@ -60,37 +61,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content={metadata.description ?? ""} />
-        <meta
-          name="keywords"
-          content={
-            Array.isArray(metadata.keywords) ? metadata.keywords.join(", ") : ""
-          }
-        />
-
-        <meta property="og:title" content={String(metadata.title) ?? ""} />
-        <meta property="og:description" content={metadata.description ?? ""} />
-        <meta
-          property="og:url"
-          content={
-            metadata.openGraph?.url ? metadata.openGraph.url.toString() : ""
-          }
-        />
-        <meta
-          property="og:image"
-          content="https://www.taehoonlee.dev/taehoon.png"
-        />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@your_twitter_handle" />
-        <meta name="twitter:title" content={String(metadata.title) ?? ""} />
-        <meta name="twitter:description" content={metadata.description ?? ""} />
-        <meta
-          name="twitter:image"
-          content="https://www.taehonlee.dev/taehoon.png"
-        />
-      </head>
       <body className={`${ABCFavorit.normal.className} antialiased`}>
         {children}
         <Analytics />
