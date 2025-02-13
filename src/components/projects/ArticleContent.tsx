339442c9e7821679fcbd64b1d5a40330364f803a
Ethan Lee <48551278+ethn1ee@users.noreply.github.com>
2025-02-14 04:10:59 +0900

    Update ArticleContent.tsx

--- src/components/projects/ArticleContent.tsx
+++ src/components/projects/ArticleContent.tsx
@@ -7,7 +7,7 @@ const ArticleContent = ({ contentHtml }: ArticleContentProps) => {
     <div className="flex justify-center px-4 py-16">
       {contentHtml.length > 0 ? (
         <article
-          className="w-1/2 max-w-5xl"
+          className="w-full sm:w-1/2 max-w-5xl"
           dangerouslySetInnerHTML={{ __html: contentHtml }}
         />
       ) : (
