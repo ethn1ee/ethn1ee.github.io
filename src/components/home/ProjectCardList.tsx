5fffa2625b44eae0395b93ad8a3d0619ade81678
Ethan Lee <ethan.lee@emory.edu>
2025-02-14 13:15:28 -0500

    Refactor Projects component to use ProjectCardList for improved structure and animation

---
+++ src/components/home/ProjectCardList.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import type { Project } from "@/types/project";
+import { stagger, useAnimate, useInView } from "motion/react";
+import { useEffect } from "react";
+
+import ProjectCard from "./ProjectCard";
+
+interface ProjectCardListProps {
+  projects: Project[];
+}
+
+const ProjectCardList = ({ projects }: ProjectCardListProps) => {
+  const [scope, animate] = useAnimate();
+  const isInView = useInView(scope, { once: true });
+
+  useEffect(() => {
+    console.log(isInView);
+    if (isInView) {
+      animate(
+        "li",
+        { opacity: 1 },
+        {
+          duration: 0.8,
+          ease: "easeOut",
+          delay: stagger(0.1, { startDelay: 0.5, ease: "easeOut" }),
+        },
+      );
+    } else {
+      animate(
+        "li",
+        { opacity: 0 },
+        {
+          duration: 0.8,
+          ease: "easeOut",
+        },
+      );
+    }
+  }, [isInView, animate]);
+
+  return (
+    <ul ref={scope} className="relative top-6 ml-0 flex flex-col sm:ml-10">
+      {projects.map((project, index) => (
+        <li key={index} className="opacity-0">
+          <ProjectCard project={project} index={index} />
+        </li>
+      ))}
+      <div className="absolute bottom-0 left-0 h-[1px] w-full bg-gray-300" />
+    </ul>
+  );
+};
+
+export default ProjectCardList;
--- src/components/home/sections/Projects.tsx
+++ src/components/home/sections/Projects.tsx
@@ -1,20 +1,13 @@
 import { getAllProjects } from "@/lib/projects";
 
-import ProjectCard from "../ProjectCard";
+import ProjectCardList from "../ProjectCardList";
 
 const Projects = async () => {
   const projects = await getAllProjects();
 
   return (
     <section id="projects" className="h-screen w-screen">
-      <ul className="relative ml-0 sm:ml-10 flex flex-col pt-6">
-        {projects.map((project, index) => (
-          <li key={index}>
-            <ProjectCard project={project} index={index} />
-          </li>
-        ))}
-        <div className="absolute bottom-0 left-0 h-[1px] w-full bg-gray-300" />
-      </ul>
+      <ProjectCardList projects={projects} />
     </section>
   );
 };
