f6f2d659a25e4deeecda405fe93ca260c1f081ec
Ethan Lee <ethan.lee@emory.edu>
2025-04-12 14:54:53 -0400

    Migrate rewrite rules from vercel.json to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/project/gameoflife/",
+        destination: "https://canvas-gameoflife.vercel.app/",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
